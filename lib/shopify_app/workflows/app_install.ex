bdbd231e37e08f024bd2bc97d669523191680822
Hez Ronningen <code@hez.ca>
2026-04-11 15:36:48 -0700

    fixing up app install and uninstall
    - removing webhook creation from job, this is now in the shopify toml file

--- lib/shopify_app/workflows/app_install.ex
+++ lib/shopify_app/workflows/app_install.ex
@@ -1,7 +1,8 @@
 defmodule ShopifyApp.Workflow.AppInstall do
+  @moduledoc """
+  The AppInstall job handles all the install details after the tokens have been exchanged.
+  """
   require Logger
-
-  alias ShopifyApp.Worker.Install.CreateWebhook
 
   @type t() :: %__MODULE__{myshopify_domain: String.t()}
   @enforce_keys [:myshopify_domain]
@@ -9,8 +10,6 @@ defmodule ShopifyApp.Workflow.AppInstall do
 
   @spec call(t()) :: :ok
   def call(%{myshopify_domain: myshopify_domain} = context) when is_struct(context, __MODULE__) do
-    Enum.each(ShopifyApp.Config.shop_webhooks(), &CreateWebhook.enqueue(myshopify_domain, &1))
-
     Logger.debug("New install", myshopify_domain: myshopify_domain)
     :ok
   end
